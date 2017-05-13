77dcfacf163fe6d01576ac385fac07a99f23e50c
Pack Heng <pack@oz-heng.com>
2017-05-14 08:17:50 +1000

    XML layouts and floating action button for Catalog and Editor activities  On branch master  Changes committed: 	modified:   .idea/misc.xml 	new file:   .idea/vcs.xml 	modified:   app/build.gradle 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/com/oz_heng/apps/android/inventory/CatalogActivity.java 	new file:   app/src/main/java/com/oz_heng/apps/android/inventory/EditorActivity.java 	new file:   app/src/main/java/com/oz_heng/apps/android/inventory/Product/ProductContract.java 	new file:   app/src/main/java/com/oz_heng/apps/android/inventory/Product/ProductCursorAdapter.java 	new file:   app/src/main/java/com/oz_heng/apps/android/inventory/Product/ProductDbHelper.java 	new file:   app/src/main/java/com/oz_heng/apps/android/inventory/Product/ProductProvider.java 	new file:   app/src/main/res/drawable-hdpi/ic_add_a_photo_black_48dp.png 	new file:   app/src/main/res/drawable-hdpi/ic_ecommerce_cash_register.png 	new file:   app/src/main/res/drawable-hdpi/ic_finance_purchase_order.png 	new file:   app/src/main/res/drawable-hdpi/ic_minus_circle_black_48dp.png 	new file:   app/src/main/res/drawable-hdpi/ic_plus_circle_black_48dp.png 	new file:   app/src/main/res/drawable-mdpi/ic_add_a_photo_black_48dp.png 	new file:   app/src/main/res/drawable-mdpi/ic_ecommerce_cash_register.png 	new file:   app/src/main/res/drawable-mdpi/ic_finance_purchase_order.png 	new file:   app/src/main/res/drawable-mdpi/ic_minus_circle_black_48dp.png 	new file:   app/src/main/res/drawable-mdpi/ic_plus_circle_black_48dp.png 	new file:   app/src/main/res/drawable-xhdpi/ic_add_a_photo_black_48dp.png 	new file:   app/src/main/res/drawable-xhdpi/ic_ecommerce_cash_register.png 	new file:   app/src/main/res/drawable-xhdpi/ic_finance_purchase_order.png 	new file:   app/src/main/res/drawable-xhdpi/ic_minus_circle_black_48dp.png 	new file:   app/src/main/res/drawable-xhdpi/ic_plus_circle_black_48dp.png 	new file:   app/src/main/res/drawable-xxhdpi/ic_add_a_photo_black_48dp.png 	new file:   app/src/main/res/drawable-xxhdpi/ic_ecommerce_cash_register.png 	new file:   app/src/main/res/drawable-xxhdpi/ic_finance_purchase_order.png 	new file:   app/src/main/res/drawable-xxhdpi/ic_minus_circle_black_48dp.png 	new file:   app/src/main/res/drawable-xxhdpi/ic_plus_circle_black_48dp.png 	new file:   app/src/main/res/drawable-xxxhdpi/ic_add_a_photo_black_48dp.png 	new file:   app/src/main/res/drawable-xxxhdpi/ic_ecommerce_cash_register.png 	new file:   app/src/main/res/drawable-xxxhdpi/ic_finance_purchase_order.png 	new file:   app/src/main/res/drawable-xxxhdpi/ic_minus_circle_black_48dp.png 	new file:   app/src/main/res/drawable-xxxhdpi/ic_plus_circle_black_48dp.png 	modified:   app/src/main/res/layout/activity_catalog.xml 	new file:   app/src/main/res/layout/activity_editor.xml 	modified:   app/src/main/res/values/colors.xml 	new file:   app/src/main/res/values/dimens.xml 	modified:   app/src/main/res/values/strings.xml 	modified:   app/src/main/res/values/styles.xml 	modified:   build.gradle

--- app/src/main/java/com/oz_heng/apps/android/inventory/CatalogActivity.java
+++ app/src/main/java/com/oz_heng/apps/android/inventory/CatalogActivity.java
@@ -1,7 +1,10 @@
 package com.oz_heng.apps.android.inventory;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
+import android.view.View;
 
 public class CatalogActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class CatalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+        // Setup Add Button to open EditorActivity
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.catalog_plus_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/oz_heng/apps/android/inventory/EditorActivity.java
@@ -0,0 +1,24 @@
+package com.oz_heng.apps.android.inventory;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Pack Heng on 12/05/17
+ * pack@oz-heng.com
+ */
+
+public class EditorActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_editor);
+
+    }
+
+    // TODO EditorActivity
+
+}
